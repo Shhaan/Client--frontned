d5c703b2432fe09f245c38b47b43a819ca0b71be
Shhaan <mohammedshan464@gmail.com>
2024-11-21 14:04:23 +0530

    sdf

--- src/Pages/Cutomer/Categoryproduct.jsx
+++ src/Pages/Cutomer/Categoryproduct.jsx
@@ -211,27 +211,15 @@ export const Categoryproduct = () => {
         </div>
       ) : (
         <div
-          style={
-            viewportheight > 720
-              ? {
-                  display: "flex",
-                  alignItems: "center",
-                  flexWrap: "wrap",
-                  marginTop: "23px",
-                  marginLeft: "25px",
-                  gap: "50px",
-                  marginBottom: "100px",
-                }
-              : {
-                  display: "flex",
-                  alignItems: "center",
-                  flexWrap: "wrap",
-                  marginTop: "23px",
-                  marginLeft: "25px",
-                  gap: "50px",
-                  marginBottom: "200px",
-                }
-          }
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "wrap",
+            marginTop: "23px",
+            marginLeft: "25px",
+            gap: "50px",
+            marginBottom: "200px",
+          }}
         >
           <div className={style.shopbycategory} style={{ margin: "0" }}>
             <div className={style.categoryNavigation}>
